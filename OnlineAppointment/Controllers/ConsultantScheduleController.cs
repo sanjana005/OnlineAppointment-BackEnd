a46b57fb7da0f86967d07fd5c8068b3e1bc7e924
sanjana005 <sanjananimradha615@gmail.com>
2023-09-04 17:05:47 +0530

    Consultant Schedule Functions

---
+++ OnlineAppointment/Controllers/ConsultantScheduleController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineAppointment.Model;
+using System.Data.SqlClient;
+
+namespace OnlineAppointment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultantScheduleController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConsultantScheduleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("AddSchedule")]
+
+        public Response AddSchedule(Schedule addSchedule)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DBCon").ToString());
+            Dal dal = new Dal();
+            response = dal.AddSchedule(addSchedule, connection);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("ScheduleList")]
+
+        public Response ScheduleList(Schedule scheduleList)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DBCon").ToString());
+            Dal dal = new Dal();
+            response = dal.ScheduleList(scheduleList, connection);
+            return response;
+        }
+
+        [HttpPut]
+        [Route("ScheduleUpdate")]
+
+        public Response ScheduleUpdate(Schedule scheduleUpdate)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DBCon").ToString());
+            Dal dal = new Dal();
+            response = dal.ScheduleUpdate(scheduleUpdate, connection);
+            return response;
+        }
+
+        [HttpDelete]
+        [Route("DeleteSchedule")]
+
+        public Response DeleteSchedule(Schedule scheduleDelete)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DBCon").ToString());
+            Dal dal = new Dal();
+            response = dal.DeleteSchedule(scheduleDelete, connection);
+            return response;
+        }
+    }
+}
